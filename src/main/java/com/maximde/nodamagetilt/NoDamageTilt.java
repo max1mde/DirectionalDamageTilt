7a592e5609ad7ae8b5e635e163a7aadf588f179b
BuildTools <unconfigured@null.spigotmc.org>
2023-05-27 20:30:11 +0300

    Initial Commit

--- src/main/java/com/maximde/nodamagetilt/NoDamageTilt.java
+++ src/main/java/com/maximde/nodamagetilt/NoDamageTilt.java
@@ -1,2 +1,21 @@
-package com.maximde.nodamagetilt;public class NoDamageTilt {
+/*
+ * By MaximDe
+ */
+
+package com.maximde.nodamagetilt;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoDamageTilt extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        PacketListener hurtAnimation = new PacketListener(this);
+        ProtocolLibrary.getProtocolManager().addPacketListener(hurtAnimation);
+    }
+
+    @Override
+    public void onDisable() {
+        ProtocolLibrary.getProtocolManager().removePacketListeners(this);
+    }
 }
--- src/main/java/com/maximde/nodamagetilt/PacketListener.java
+++ src/main/java/com/maximde/nodamagetilt/PacketListener.java
@@ -1,2 +1,33 @@
-package com.maximde.nodamagetilt;public class PacketListener {
+/*
+ * By MaximDe
+ */
+
+package com.maximde.nodamagetilt;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import net.minecraft.network.protocol.game.ClientboundHurtAnimationPacket;
+
+
+public class PacketListener extends PacketAdapter {
+
+    public PacketListener(NoDamageTilt noDamageTilt) {
+        super(noDamageTilt, ListenerPriority.NORMAL, PacketType.Play.Server.HURT_ANIMATION);
+    }
+
+    @Override
+    public void onPacketReceiving(PacketEvent event) {}
+
+    @Override
+    public void onPacketSending(PacketEvent event) {
+        if (event.getPacket().getType() == PacketType.Play.Server.HURT_ANIMATION) {
+            float modifiedYaw = -180.0f;
+            ClientboundHurtAnimationPacket modifiedPacket = new ClientboundHurtAnimationPacket(0, modifiedYaw);
+            event.setPacket(PacketContainer.fromPacket(modifiedPacket));
+        }
+    }
 }
+
